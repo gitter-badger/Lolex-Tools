b14b0215af81ead3373e731852568469b2c039b7
Monkeyboy2805 <Monkeyboy2805@gmail.com>
2016-08-17 14:38:44 +0100

    Trying trying trying ouch!

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="LolexTools"
+    version="0.0.1",
+    description="N/A",
+    py_modules=["JTToolsInstaller, JTToolsMethods"],
+    install_requires=["Twisted"]
+)
