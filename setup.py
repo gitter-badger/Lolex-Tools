499e659250a44857f3e1347f6c40d1bc31a14194
Monkeyboy2805 <Monkeyboy2805@gmail.com>
2016-08-17 14:58:58 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,5 @@ from setuptools import setup
 
 setup(
     name =("LolexTools")
-    description="N/A",
-    py_modules=["JTToolsInstaller, JTToolsMethods"],
-    install_requires=["Twisted"]
+    
 )
